ca4c28a694a603a4c91a7c80287009801f306bd9
limkskr <113366171+limkskr@users.noreply.github.com>
2024-01-08 14:52:49 +0000

    24.01.08

---
+++ .ccls-cache/@home@runner@cpp/CLinkedList.h
@@ -0,0 +1,13 @@
+#ifndef __C_LINKED_LIST__
+#define __C_LINKED_LIST__
+
+#define TRUE    1
+#define FALSE    0
+
+typedef int Data;
+
+typedef struct _node
+{
+    Data data
+    struct _node * next
+}
