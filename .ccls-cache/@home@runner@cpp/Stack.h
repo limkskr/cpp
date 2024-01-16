c918bac96fc02e725a9f784a7d6bef305fdd3ea5
limkskr <113366171+limkskr@users.noreply.github.com>
2024-01-16 14:47:09 +0000

    24.01.16 (Stack1)

---
+++ .ccls-cache/@home@runner@cpp/Stack.h
@@ -0,0 +1,7 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#define TRUE     1
+#define FALSE    0
+
+struct
--- main.cpp
+++ main.cpp
@@ -1,46 +1,18 @@
+#include "ListBaseStack.h"
 #include <iostream>
-#include "DBDLinkedList.h"
 
 int main(void)
 {
-    List list;
-    int data;
-    ListInit(&list);
-    
-    LInsert(&list, 1); LInsert(&list, 2);
-    LInsert(&list, 3); LInsert(&list, 4);
-    LInsert(&list, 5); LInsert(&list, 6);
-    LInsert(&list, 7); LInsert(&list, 8);
-    
-    if(LFirst(&list, &data))
-    {
-        printf("%d ", data);
-        
-        while(LNext(&list, &data))
-            printf("%d ", data);
-        printf("\n");
-    }
-    
-    if(LFirst(&list, &data))
-    {
-        if(data%2 == 0)
-            LRemove(&list);
-        
-        while(LNext(&list, &data))
-        {    
-            if(data%2 == 0)
-                LRemove(&list);
-        }
-    }
+    Stack stack;
+    StackInit(&stack);
 
-    if(LFirst(&list, &data))
+    SPush(&stack, 1); SPush(&stack, 2);
+    SPush(&stack, 3); SPush(&stack, 4);
+    SPush(&stack, 5);
+
+    while(!SIsEmpty(&stack))
     {
-        printf("%d ", data);
-
-        while(LNext(&list, &data))
-            printf("%d ", data);
-
-        printf("\n");
+        printf("%d ", SPop(&stack));
     }
     return 0;
 }
